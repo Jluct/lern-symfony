10fd3778496ec0b87a8b9e722314c8d86fcbfca2
Jluct <panaev02@gmail.com>
2017-02-07 23:51:42 +0300

    add mat-card snippet

--- src/AppBundle/Resources/public/js/index.js
+++ src/AppBundle/Resources/public/js/index.js
@@ -5,5 +5,14 @@
 
 
 $(function(){
-    $.material.init()
+    $.material.init();
+
+    var options = {
+        icon_close     : 'fa-arrow-left', //string
+        icon_open      : 'fa-bars',       //string
+        icon_spin      : 'fa-spin-fast',  //string
+        card_activator : 'click'          //string: click or hover
+    };
+
+    $('.material-card').materialCard(options);
 });
